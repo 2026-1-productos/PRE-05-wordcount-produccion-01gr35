055401704cfd3d61afe07bca1f4475c24308b91e
53rg10 <semontoyape@unal.edu.co>
2026-05-23 10:30:43 -0500

    save

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -6,9 +6,10 @@ import os
 def test_word_count():
     """Test Word Count"""
 
-    for path in [
-        "Dockerfile",
-        ".dockerignore",
-    ]:
-        if not os.path.exists(path):
-            raise Exception(f"'{path}' directory does not exist")
+
+for path in [
+    "Dockerfile",
+    ".dockerignore",
+]:
+    if not os.path.exists(path):
+        raise Exception(f"'{path}' directory does not exist")
